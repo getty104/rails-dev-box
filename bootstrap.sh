1bc7accccb123055f620b4386dcfeb5a37165615
getty104 <hayabusatoshihumi@gmail.com>
2018-05-10 14:23:52 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,9 +22,7 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 
 apt-get -y update >/dev/null 2>&1
 
-install 'development tools' build-essential autoconf libtool mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 nodejs
-
-install Yarn yarn
+install 'development tools' build-essential autoconf libtool mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8
 
 install Ruby ruby2.5 ruby2.5-dev
 update-alternatives --set ruby /usr/bin/ruby2.5 >/dev/null 2>&1
@@ -44,38 +42,16 @@ install RabbitMQ rabbitmq-server
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
-sudo -u postgres createdb -O vagrant activerecord_unittest
-sudo -u postgres createdb -O vagrant activerecord_unittest2
 
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 install MySQL mysql-server libmysqlclient-dev
-# Set the password in an environment variable to avoid the warning issued if set with `-p`.
-MYSQL_PWD=root mysql -uroot <<SQL
-CREATE USER 'rails'@'localhost';
-CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
-SQL
 
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
-install 'Blade dependencies' libncurses5-dev
+
 install 'ExecJS runtime' nodejs
 
-# To generate guides in Kindle format.
-install 'ImageMagick' imagemagick
-echo installing KindleGen
-kindlegen_tarball=kindlegen_linux_2.6_i386_v2_9.tar.gz
-wget -q http://kindlegen.s3.amazonaws.com/$kindlegen_tarball
-tar xzf $kindlegen_tarball kindlegen
-mv kindlegen /usr/local/bin
-rm $kindlegen_tarball
-
-install 'MuPDF' mupdf mupdf-tools
-install 'FFmpeg' ffmpeg
-install 'Poppler' poppler-utils
+install Yarn yarn
 
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
